b5eb34f1e82985a9dca7204ca6ac8240291945b7
Ruari Douglas <ruaridouglas@ruaris-imac.home>
2020-09-14 15:09:04 +0200

    Add month selection functionality

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
-
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 // ----------------------------------------//
-const date = new Date(),
-    day = date.getDate(),
-    month = date.toLocaleString('default', { month: 'long' });
+require('dotenv').config();
+let DB_URL = process.env.DATABASE_URL;
+// ----------------------------------------//
+const date = new Date();
+const day = date.getDate();
+const month = date.toLocaleString('default', { month: 'long' });
 
 
 
@@ -15,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.listen(3000, () => console.log("I'm listening"));
-mongoose.connect('mongodb://localhost:27017/hours_calculator', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const monthSchema = new mongoose.Schema({
     month: String,
@@ -35,30 +37,42 @@ const shiftSchema = new mongoose.Schema({
 });
 const Shift = mongoose.model('Shift', shiftSchema);
 
+// Month.create({ month: 'December' }, (err, newMonth) => {
+//     if (err) { console.log(err) } else { console.log(`${newMonth.month} added`) }
+// });
 
+// INDEX ROUTE
 app.get('/', (req, res) => {
-    Shift.find({}, (err, allShifts) => {
-        if (err) console.log(`There's an error: ${err}`)
-        else res.render('index', {
-            shifts: allShifts,
-            day: day,
-            month: month
-        })
-    });
     Month.find({ month: { $in: month } }, (err, foundMonth) => {
+        console.log(foundMonth)
         if (foundMonth.length === 0) {
             Month.create({ month: month }, (err, createdMonth) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    return;
+                    res.redirect(`/${createdMonth._id}`)
                 }
             });
+        } else {
+            res.redirect(`/${foundMonth[0]._id}`);
         }
     });
 });
 
-app.post('/', (req, res) => {
+
+app.get('/:id', (req, res) => {
+    Month.find({}, (err, allMonths) => {
+        Month.findById(req.params.id).populate('shifts').exec((err, foundMonth) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('show', { foundMonth: foundMonth, allMonths: allMonths })
+            }
+        })
+    })
+});
+
+app.post('/:id', (req, res) => {
     const startTime = Number(req.body.startTime);
     const finishTime = Number(req.body.endTime);
     const lunchTime = Number(req.body.lunchTime);
@@ -70,17 +84,17 @@ app.post('/', (req, res) => {
         lunchTime: lunchTime,
         totalShiftHours: totalShiftHours
     };
-
-    Month.find({ month: { $in: month } }, (err, newFoundMonth) => {
-        Shift.create(newShift, (err, createdShift) => {
+    Month.findById(req.params.id, (err, foundMonth) => {
+        Shift.create(newShift, (err, newShift) => {
             if (err) {
-                console.log(`Cant create the shift ${err}`)
+                console.log(err)
             } else {
-                createdShift.save();
-                newFoundMonth[0].shifts.push(createdShift);
-                newFoundMonth[0].save();
-                res.redirect('/');
+                newShift.save();
+                foundMonth.shifts.push(newShift);
+                foundMonth.save();
+                res.redirect('back')
             }
         })
+
     });
 });
