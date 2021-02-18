36b19a21ce1ea3f0b4a0765cae7360f0e8e4bdec
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-02-18 15:42:02 +0100

    Fix total hours bug

--- routes/months.js
+++ routes/months.js
@@ -1,63 +1,72 @@
 // ----------- Packages/ENV ---------------//
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const functions = require('../functions');
-const methodOverride = require('method-override');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const functions = require("../functions");
+const methodOverride = require("method-override");
 
 // ----------- Models ---------------//
-const Month = require('../models/month');
-const Shift = require('../models/shift');
+const Month = require("../models/month");
+const Shift = require("../models/shift");
 
 // ----------- Variables ---------------//
 const newDate = new Date();
 const day = newDate.getDate();
-const month = newDate.toLocaleString('default', { month: 'long' });
+const month = newDate.toLocaleString("default", { month: "long" });
 
 // ----------- Routes ---------------//
 // SHOW
-router.get('/:id', functions.isLoggedIn, (req, res) => {
-    Month.find({}, (err, allMonths) => {
-        Month.findById(req.params.id).populate({ path: 'shifts', options: { sort: { date: 1 } } }).exec((err, foundMonth) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('show', { month: foundMonth, allMonths: allMonths, functions: functions })
-            }
-        })
-    });
+router.get("/:id", functions.isLoggedIn, (req, res) => {
+  Month.find({}, (err, allMonths) => {
+    Month.findById(req.params.id)
+      .populate({ path: "shifts", options: { sort: { date: 1 } } })
+      .exec((err, foundMonth) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("show", {
+            month: foundMonth,
+            allMonths: allMonths,
+            functions: functions,
+          });
+        }
+      });
+  });
 });
 // CREATE (NEW SHIFT IN MONTH)
-router.post('/:id', functions.isLoggedIn, (req, res) => {
-    const userDay = req.body.date;
-    const startTime = functions.slicer(req.body.startTime);
-    const finishTime = functions.slicer(req.body.endTime);
-    const lunchTime = functions.slicer(req.body.lunchTime);
-    const totalShiftHours = functions.totalHours(startTime, finishTime, lunchTime);
-    const newShift = {
-        date: userDay || day,
-        startTime: startTime,
-        finishTime: finishTime,
-        lunchTime: lunchTime,
-        totalShiftHours: totalShiftHours
-    };
-    Month.findById(req.params.id, (err, foundMonth) => {
-        Shift.create(newShift, (err, shift) => {
-            if (err) {
-                req.flash('error', error.message);
-                console.log(err)
-            } else {
-                shift.author.id = req.user._id;
-                shift.author.username = req.user.username;
-                shift.save();
-                foundMonth.shifts.push(shift);
-                foundMonth.save();
-                req.flash('success', `New shift added!`)
-                res.redirect('back');
-            }
-        })
+router.post("/:id", functions.isLoggedIn, (req, res) => {
+  const userDay = req.body.date;
+  const startTime = functions.slicer(req.body.startTime);
+  const finishTime = functions.slicer(req.body.endTime);
+  const lunchTime = functions.slicer(req.body.lunchTime);
+  let totalShiftHours = functions.totalHours(startTime, finishTime, lunchTime);
+  if (+totalShiftHours < 0) {
+    totalShiftHours = 0;
+  }
 
+  const newShift = {
+    date: userDay || day,
+    startTime: startTime,
+    finishTime: finishTime,
+    lunchTime: lunchTime,
+    totalShiftHours: totalShiftHours,
+  };
+  Month.findById(req.params.id, (err, foundMonth) => {
+    Shift.create(newShift, (err, shift) => {
+      if (err) {
+        req.flash("error", error.message);
+        console.log(err);
+      } else {
+        shift.author.id = req.user._id;
+        shift.author.username = req.user.username;
+        shift.save();
+        foundMonth.shifts.push(shift);
+        foundMonth.save();
+        req.flash("success", `New shift added!`);
+        res.redirect("back");
+      }
     });
+  });
 });
 module.exports = router;
